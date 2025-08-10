60ecd4bf16ba6fd19c563b551dcf6ce9006c19b0
SKINPALL <s6701012610040@email.kmutnb.ac.th>
2025-08-10 14:09:26 +0700

    create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  // ตัวแปร หรือ ฟังก์ชันที่นี้
+
+  return (
+    <>
+      
+      {/* html ในนี้ */}
+
+    </>
+  )
+}
+
+export default App
