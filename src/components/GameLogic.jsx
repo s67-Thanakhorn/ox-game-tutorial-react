1ab9215be0221093bf1c9039c1541fded952873e
SKINPALL <s6701012610040@email.kmutnb.ac.th>
2025-08-12 17:23:00 +0700

    Check win condition

--- src/components/GameLogic.jsx
+++ src/components/GameLogic.jsx
@@ -2,36 +2,73 @@ import React, { useEffect, useRef, useState } from 'react'
 
 function GameLogic() {
 
-    const [turn, setTurn] = useState('X');
+    // ตัวแปรเทิร์น
+    const [turn, setTurn] = useState('O');
 
+    // ตัวแปรตาราง XO
     const [table, setTable] = useState([['', '', ''],
-                                        ['', '', ''],
-                                        ['', '', '']]);
+    ['', '', ''],
+    ['', '', '']]);
 
-
+    // อ้างอิง id จาก canvas แทนการใช้ getElementById
     const canvasRef = useRef(null);
+
+    // อ้างอิงตัวแปร ctx ใน useEffect เนื่องจากเป็น local 
     const ctxRef = useRef(null);
 
+    // กำหนดการคลิกเมื่อจบเกม ถ้าเป็น true = กดได้ , flase = กดไม่ได้ (หลังเกมจบ)
+    const [click,setClick] = useState(true);
+    
     useEffect(() => {
         const c = canvasRef.current;
         const ctx = c.getContext("2d");
         ctxRef.current = ctx;
     }, []);
 
+    // จะทำต่อเมื่อ table มีการเปลี่ยนค่า
+    useEffect(() => {
+
+        // ถ้า winCheck return = true คือมีคนชนะ
+        if (winCheck()) {
+
+            alert(`Player ${turn} wins!`);
+            setClick(false);
+        
+        // เช็คว่าเต็มไม่มีช่องว่าง = draw
+        } else if (table.flat().every(cell => cell !== '')) {
+             
+            alert('Draw');
+            setClick(false);
+        
+        // ถ้าเข้าเงื่อนไชก็เปลี่ยนเทิร์น
+        }else {
+
+            changeTurn();
+        }
+
+
+    }, [table]);
+
+    // ฟังก์ชัน รับมือการกด
     const handleClick = e => {
 
+        // รับค่าพิกัดของเมาส์แล้วมาเปลงเป็น row , column
         let row = Math.floor(e.nativeEvent.offsetX / 200);
         let col = Math.floor(e.nativeEvent.offsetY / 200);
 
-        console.log(table);
-        changeTurn();
-        draw(row,col);
+
+        if (click) {
+
+            draw(row, col);
+
+        }
 
     };
 
+    // ฟังก์ชัน เปลี่ยนเทิร์น
     const changeTurn = () => {
 
-        if (turn === 'X' ) {
+        if (turn === 'X') {
 
             setTurn('O');
 
@@ -43,6 +80,7 @@ function GameLogic() {
 
     };
 
+    // ฟังก์ชันวาด XO ในตาราง 
     const draw = (row, col) => {
         const ctx = ctxRef.current;
         const newTable = table.map(row => [...row]);
@@ -50,27 +88,27 @@ function GameLogic() {
         ctx.beginPath();
         ctx.strokeStyle = "#f39899ff";
         ctx.lineWidth = 7;
-        
+
 
         if (turn === 'O' && table[row][col] === '') {
 
             newTable[row][col] = turn;
             setTable(newTable);
 
-            ctx.arc((row*200)+100,(col*200)+100,70,0,2*Math.PI);
+            ctx.arc((row * 200) + 100, (col * 200) + 100, 70, 0, 2 * Math.PI);
             ctx.stroke();
 
-        }else if (turn === 'X' && table[row][col] === '') {
+        } else if (turn === 'X' && table[row][col] === '') {
 
             newTable[row][col] = turn;
             setTable(newTable);
 
-            ctx.moveTo(((row)*200)+30,((col)*200)+30);
-            ctx.lineTo(((row+1)*200)-30,((col+1)*200)-30);
+            ctx.moveTo(((row) * 200) + 30, ((col) * 200) + 30);
+            ctx.lineTo(((row + 1) * 200) - 30, ((col + 1) * 200) - 30);
 
-            ctx.moveTo(((row+1)*200)-30,((col)*200)+30);
-            ctx.lineTo(((row)*200)+30,((col+1)*200)-30);
-            
+            ctx.moveTo(((row + 1) * 200) - 30, ((col) * 200) + 30);
+            ctx.lineTo(((row) * 200) + 30, ((col + 1) * 200) - 30);
+
 
             ctx.stroke();
 
@@ -78,11 +116,55 @@ function GameLogic() {
 
     }
 
+    // ฟังก์ชันเช็คเงื่อนไขการชนะ
+    const winCheck = () => {
+
+        let turnValue = ['X', 'O'];
+
+        for (let i = 0; i < turnValue.length; i++) {
+
+            if (table[0][0] === turnValue[i] && table[0][1] === turnValue[i] && table[0][2] === turnValue[i]) {
+
+                return true;
+
+            } else if (table[1][0] === turnValue[i] && table[1][1] === turnValue[i] && table[1][2] === turnValue[i]) {
+
+                return true;
+
+            } else if (table[2][0] === turnValue[i] && table[2][1] === turnValue[i] && table[2][2] === turnValue[i]) {
+
+                return true;
+
+            } else if (table[0][0] === turnValue[i] && table[1][0] === turnValue[i] && table[2][0] === turnValue[i]) {
+
+                return true;
+
+            } else if (table[0][1] === turnValue[i] && table[1][1] === turnValue[i] && table[2][1] === turnValue[i]) {
+
+                return true;
+
+            } else if (table[0][2] === turnValue[i] && table[1][2] === turnValue[i] && table[2][2] === turnValue[i]) {
+
+                return true;
+
+            } else if (table[0][0] === turnValue[i] && table[1][1] === turnValue[i] && table[2][2] === turnValue[i]) {
+
+                return true;
+
+            } else if (table[0][2] === turnValue[i] && table[1][1] === turnValue[i] && table[2][0] === turnValue[i]) {
+
+                return true;
+
+            }
+
+        }
+
+    }
 
     return (
         <>
             <canvas
-                ref = {canvasRef}
+                ref={canvasRef}
                 id="myDrawCanvas"
                 width="600"
                 height="600"
