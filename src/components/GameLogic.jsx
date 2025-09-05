a61850cd59a12ab2968a5fa0e1a542a4c0d4c0e6
s67-Chakkapong <s6701012610155@email.kmutnb.ac.th>
2025-09-05 10:19:33 +0700

    change drawMarks

--- src/components/GameLogic.jsx
+++ src/components/GameLogic.jsx
@@ -54,7 +54,7 @@ function GameLogic({gridProps}) {
             setClick(false);
         }
 
-        setTurn(turn === 'O' ? 'X' : 'O');
+        setTurn(turn === 'S' ? 'T' : 'S');
     }, [table]);
 
     const handleClick = e => {
@@ -86,16 +86,26 @@ function GameLogic({gridProps}) {
         for (let row = 0; row < gridCount; row++) {
             for (let col = 0; col < gridCount; col++) {
                 const mark = table[row][col];
-                if (mark === 'O') {
+                if (mark === 'S') {
                     ctx.beginPath();
-                    ctx.arc(col * cellSize + (cellSize/2), row * cellSize + (cellSize/2), (cellSize*(1/3)), 0, 2 * Math.PI);
+                    //ctx.arc(col * cellSize + (cellSize/2), row * cellSize + (cellSize/2), (cellSize*(1/3)), 0, 2 * Math.PI);
+                    ctx.rect(col * cellSize + (cellSize*0.175), row * cellSize + (cellSize*0.175), cellSize*0.7, cellSize*0.7);
                     ctx.stroke();
-                } else if (mark === 'X') {
+                } else if (mark === 'T') {
                     ctx.beginPath();
-                    ctx.moveTo(col * cellSize + (cellSize*0.15), row * cellSize + (cellSize*0.15));
-                    ctx.lineTo(col * cellSize + (cellSize*0.85), row * cellSize + (cellSize*0.85));
-                    ctx.moveTo(col * cellSize + (cellSize*0.85), row * cellSize + (cellSize*0.15));
-                    ctx.lineTo(col * cellSize + (cellSize*0.15), row * cellSize + (cellSize*0.85));
+
+                    //เส้นฐาน
+                    ctx.moveTo(col * cellSize + (cellSize*0.15), row * cellSize + (cellSize*(3.5/4)));
+                    ctx.lineTo(col * cellSize + (cellSize*0.85), row * cellSize + (cellSize*(3.5/4)));
+
+                    //หน้าจั่ว
+                    ctx.moveTo(col * cellSize + (cellSize*0.5), row * cellSize + (cellSize*(0.75/4)));
+                    ctx.lineTo(col * cellSize + (cellSize*0.15), row * cellSize + (cellSize*(3.5/4)));
+                    ctx.moveTo(col * cellSize + (cellSize*0.5), row * cellSize + (cellSize*(0.75/4)));
+                    ctx.lineTo(col * cellSize + (cellSize*0.85), row * cellSize + (cellSize*(3.5/4)));
+                    
+
+                    
                     ctx.stroke();
                 }
             }
