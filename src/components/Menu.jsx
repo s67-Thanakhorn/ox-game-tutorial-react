b6afa69ab794d3ba3642bf5e95af05251f0cd202
SKINPALL <s6701012610040@email.kmutnb.ac.th>
2025-09-24 14:17:33 +0700

    Update Menu component html

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { supabase } from '../supabaseClient'
 
 export default function Menu({setScene}) {
   return (
     <>
-    
-        <button 
+
+        <h1 style={{color : "blue", fontSize : 60}}>XO Game</h1>
+
+        <h2>Multiplayer <button>Create room</button></h2>
+
+        <form >
+
+            <h2>Join room : <input type="text" placeholder='Enter room number' /><input type="submit" /></h2>
+            
+
+        </form>
+
+        <h2>Single player <button 
         
         onClick={() => setScene(prev => !prev)}
-        >Click to change scene</button>
+        >Play now</button></h2>
+        
 
     </>
     
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvZGViYnh3eWx6em5yaXludXhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1OTUwOTYsImV4cCI6MjA3MzE3MTA5Nn0.j91zfpa-hvvJsUCC83923uqHb0_-Y-5N2nBkBpb5DHo'
 const supabaseUrl = 'https://xodebbxwylzznriynuxl.supabase.co'
 
-export const supabase = createClient(supabaseKey, supabaseUrl)
+export const supabase = createClient(supabaseUrl, supabaseKey )
